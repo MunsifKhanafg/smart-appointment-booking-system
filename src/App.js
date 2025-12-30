0fff3e747d5b2f1740c417bc890a84f22737fd56
Munsif Khan <munsifkhanafg@gmial.com>
2025-12-30 22:21:05 +0500

    fix some error

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AlertProvider } from './modules/AlertModule';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import BookingPage from './pages/BookingPage';
 import AdminPage from './pages/AdminPage';
-import AlertDemoPage from './pages/AlertDemoPage';
+
 import './App.css';
 
 function App() {
   return (
-    <AlertProvider>
+    
       <Router>
         <div className="app">
           <Header />
@@ -20,13 +20,12 @@ function App() {
               <Route path="/" element={<HomePage />} />
               <Route path="/booking/:doctorId" element={<BookingPage />} />
               <Route path="/admin" element={<AdminPage />} />
-              <Route path="/alert-demo" element={<AlertDemoPage />} />
             </Routes>
           </main>
           <Footer />
         </div>
       </Router>
-    </AlertProvider>
+  
   );
 }
 
